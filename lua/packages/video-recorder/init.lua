4a9f1e98f2e2893b915ed7b7675e82279feaed7e
PrikolMen <olegis019@gmail.com>
2023-05-22 04:18:36 +0400

    Updated

--- lua/packages/video-recorder/init.lua
+++ lua/packages/video-recorder/init.lua
@@ -1,4 +1,4 @@
-import( gpm.PackageExists( "packages/glua-extensions" ) and "packages/glua-extensions" or "https://github.com/Pika-Software/glua-extensions" )
+require( "packages/glua-extensions", "https://github.com/Pika-Software/glua-extensions" )
 
 local packageName = gpm.Package:GetIdentifier()
 local CreateClientConVar = CreateClientConVar
